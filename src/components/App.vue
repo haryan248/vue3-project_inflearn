9ed07f228d1ffb297789a76b0624e97591c7d14a
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2022-04-17 19:32:14 +0900

    04/17 TIL vue 3 문법 익히기

---
+++ src/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :class="nameClass">
+        {{ name }}
+        {{ num }}
+    </div>
+    <input type="text" v-bind:value="name" />
+    <button class="btn btn-primary" @click="updateName">CLick</button>
+    <!-- v-on == @ -->
+</template>
+<script>
+import { ref } from "vue";
+import { reactive } from "vue";
+export default {
+    setup() {
+        const name = ref("Kossie Coder1");
+        const num = reactive({
+            id: 1,
+        });
+
+        const nameClass = ref("");
+
+        const updateName = () => {
+            name.value = "Kossie Coder";
+            // ref 사용시 .value를 써야함
+            num.id = 2;
+            // reactive 사용시 바로 변경 가능
+            nameClass.value = "name";
+        };
+
+        return {
+            name,
+            num,
+            updateName,
+            nameClass,
+        };
+    },
+};
+</script>
