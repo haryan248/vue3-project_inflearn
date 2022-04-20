1155d952e8e7f75fc1d6a0de54d1998a8c71c674
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2022-04-20 19:33:11 +0900

    04/20 TIL todo mockdb, pagination 진행

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
         <div class="card-body p-2 d-flex align-items-center">
             <div class="form-check flex-grow-1">
-                <input class="form-check-input" type="checkbox" :value="todo.completed" @change="toggleTodo(index)" />
+                <input class="form-check-input" type="checkbox" :checked="todo.completed" @change="toggleTodo(index)" />
                 <label class="form-check-label" :class="{ todo: todo.completed }">
                     {{ todo.subject }}
                 </label>
@@ -22,13 +22,14 @@ export default {
             required: true,
         },
     },
-    setup(props, context) {
+    emits: ["toggle-todo", "delete-todo"],
+    setup(props, { emit }) {
         const toggleTodo = (index) => {
-            context.emit("toggle-todo", index);
+            emit("toggle-todo", index);
         };
 
         const deleteTodo = (index) => {
-            context.emit("delete-todo", index);
+            emit("delete-todo", index);
         };
         return {
             toggleTodo,
