0e705454b3e4cdfbe7859a43dab5ce4dc0cb0a76
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2022-04-29 20:47:19 +0900

    04/29 TIL Toast 컴포넌트 App 컴포넌트로 이동

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link class="navbar-brand" to="/">Hyun Joon</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" :to="{ name: 'Home'}">
+      Kossie Coder
+    </router-link>
 
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <router-link class="nav-link" to="/todos">Todos</router-link>
-            </li>
-        </ul>
-    </nav>
-    <div class="container">
-        <router-view />
-    </div>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" :to="{ name: 'Todos'}">
+          Todos
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+  <div class="container">
+      <router-view/>
+  </div>
+  <Toast 
+    v-if="showToast" 
+    :message="toastMessage"
+    :type="toastAlertType"
+  />
 </template>
 
 <script>
-export default {};
+import Toast from '@/components/Toast.vue';
+import { useToast } from '@/composables/toast';
+export default {
+  components: {
+    Toast
+  },
+  setup() {
+    const {
+      toastMessage,
+      toastAlertType,
+      showToast,
+      triggerToast
+    } = useToast();
+    return {
+      toastMessage,
+      toastAlertType,
+      showToast,
+      triggerToast
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+</style>
