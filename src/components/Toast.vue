2b1f961a16d86f382c2b042510a92b5f864884a1
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2022-05-02 20:18:01 +0900

    05/02 TIL transition-group 사용해서 toast component 변경

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,28 +1,21 @@
 <template>
-    <div class="alert toast-box" :class="`alert-${type}`" role="alert">
-        {{ message }}
+    <div class="toast-box">
+        <transition-group name="slide">
+            <div v-for="toast in toasts" :key="toast.id" class="alert" :class="`alert-${toast.type}`" role="alert">
+                {{ toast.message }}
+            </div>
+        </transition-group>
     </div>
 </template>
 
 <script>
+import { useToast } from "@/composables/toast";
 export default {
-    props: {
-        message: {
-            type: String,
-            required: true,
-        },
-        type: {
-            type: String,
-            default: "success",
-        },
+    setup() {
+        const { toasts } = useToast();
+        return {
+            toasts,
+        };
     },
 };
 </script>
-
-<style>
-.toast-box {
-    position: fixed;
-    top: 10px;
-    right: 10px;
-}
-</style>
