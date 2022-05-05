0d32920514b9189b94cf07f2dcd8759703fa8849
Hyun.Ha <51049245+haryan248@users.noreply.github.com>
2022-05-05 20:52:51 +0900

    05/05 navbar, pagination component 등록

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link class="navbar-brand" :to="{ name: 'Home' }"> Kossie Coder </router-link>
-
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <router-link class="nav-link" :to="{ name: 'Todos' }"> Todos </router-link>
-            </li>
-        </ul>
-    </nav>
+    <Navbar />
     <div class="container">
         <router-view />
     </div>
-
     <Toast />
 </template>
 
 <script>
 import Toast from "@/components/Toast.vue";
+import Navbar from "@/components/Navbar.vue";
 export default {
     components: {
         Toast,
+        Navbar,
     },
 };
 </script>
